f559c907af74d5e274e22dcf351e87d8e7c18904
Vasini25 <flavippolini@yahoo.com.br>
2023-04-18 18:29:10 -0300

    adicionada funcionalidade de calculo com fator de sazonalidade por período caso haja um mau 'r'

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import math
 
 #n = quantidade de pontos
 #j = offset (valor x) do primeiro ponto
+#proxpPontoX = valor noi eixo X ponto a ser calculado com a equação de reta encontrado (preferenciamente x+1)
 #x[] = lista de pontos x
 #y[] = lista de valores y nos pontos correspondentes
 # a quantidade de valores na Lista y deve ser igual ao valor de n
+
+#A partir de do valor 'FsOffset', considerar os 'FsBloco' primeiros valores num ciclo de 'FsCiclo' valores (default = 1, 1, 1)
 def calc():
-    n = 10
+    n = 16
     j = 1
+    proxPontoX = n+1
     x = []
-    y = [15, 18, 12, 12, 20, 14, 18, 17, 14, 20]
+    y = [11, 20, 51, 22, 13, 31, 60, 28, 12, 39, 62, 40, 23, 25, 88, 45]
+
+    FsBloco = 1
+    FsCiclo = 4
+    FsOffset = 1
 
     for i in range (n):
         x.append(i+j)
@@ -44,12 +52,11 @@ def calc():
     print("Equação da reta:\nY = {} + {}x\n".format(a, b))
 
     #calcula valor do proximo ponto
-    proxPontoX = x[n - 1] + 1
     proxPontoY = (b*proxPontoX) + a
     print("Portanto, para o proximo ponto (x = {}), estima-se:\n y = {}\n".format(proxPontoX, proxPontoY))
 
     #calcula fator de correlação
-    denominador = math.sqrt((denominador) * ((n * somYquad) - (somY **2)))
+    denominador = math.sqrt(denominador * ((n * somYquad) - (somY **2)))
     r = (nominadorB) / (denominador)
     print("Fator de correlação r = {}".format(r))
 
@@ -57,6 +64,35 @@ def calc():
         print("Bom fator de correlação!\n\n\n")
     else:
         print("Mau fator de correlação!\n\n\n")
+        P = []
+        Ft = []
+        somFt = 0
+        contador = 0
+        offset = FsOffset - 1
+
+        for i in range(n):
+            P.append(a + (b*x[i]))
+            Ft.append(y[i]/P[i])
+
+            if((contador < FsBloco) and (i >= offset)):
+                somFt += Ft[i]
+                contador += 1
+
+            if ((i+1) % FsCiclo == 0):
+                contador = 0
+                offset += FsBloco
+
+        print("Portanto, calculando o fator de sazonalidade:")
+        print("P = {}".format(P))
+        print("Ft = {}\n".format(Ft))
+        Fs = somFt/(FsCiclo * FsBloco)
+        print("Fs = {}\n".format(Fs))
+
+        DadoCorrigidoComFs = proxPontoY*Fs
+        print("Valor no ponto x = {} corrigido pelo Fator de Sazonalidade:".format(proxPontoX))
+        print(DadoCorrigidoComFs)
+        print("\n\n\n")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
